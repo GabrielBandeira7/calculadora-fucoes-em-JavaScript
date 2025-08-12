9e1afd24d4df51f2421abc378e3d8ea3a495808c
Gabriel Bandeira <bandeiragabriel07@gmail.com>
2025-08-12 10:26:46 -0300

    Adicionar

---
+++ funcao1.js
@@ -0,0 +1,8 @@
+saudacao("Gabriel");
+saudacao("Maria");
+saudacao("Carlos");
+
+function saudacao(nome) {
+    console.log(`Olá ${nome} seja bem vindo(a).`);
+}
+
---
+++ funcao2.js
@@ -0,0 +1,38 @@
+let num1, num2;
+
+let resultadoSoma = (num1 + num2);
+console.log(resultadoSoma);
+
+function somar(num1, num2) {
+    return num1 + num2;
+}
+
+let numero1, numero2;
+
+let resuladoSubtracao = (numero1 - numero2);
+console.log(resuladoSubtracao);
+
+function subtrair(numero1, numero2) {
+    return numero1 - numero2;
+}
+
+let numero_1, numero_2;
+
+let resultadoMultiplicacao = (numero_1 * numero_2);
+console.log(resultadoMultiplicacao);
+
+function multiplicar(numero_1, numero_2) {
+    return numero_1 * numero_2;
+}
+
+let numero__1, numero__2;
+
+let resultadoDivisao = (numero__1/numero__2);
+console.log(resultadoDivisao);
+
+if(numero__1, numero__2 = 0){
+    console.log(ERROR);
+}
+function dividir(numero__1, numero__2) {
+    return numero__1/numero__2;
+}
